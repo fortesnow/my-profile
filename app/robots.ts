1003d2aea843ca64a6d9f81fb077e71651e53ab6
fortesnow <fortesnow@gmail.com>
2025-03-09 05:41:06 +0900

    fixed

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://example.com/sitemap.xml',
+    sitemap: 'https://www.stellarium.jp/sitemap.xml',
   }
 } 
